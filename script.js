0119387f235e46eca70e5a8f92650e96e139115a
Moa Biru <99655212+birachis@users.noreply.github.com>
2026-01-19 15:13:34 +0300

    title fix

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const header = document.querySelector("header");
-window.addEventListener("scroll", function() {
-    header.classList.toggle ("sticky", window.scrollY > 100);
+window.addEventListener("scroll", function () {
+    header.classList.toggle("sticky", window.scrollY > 100);
 });
 
 let menu = document.querySelector('#menu-icon');
@@ -15,9 +15,9 @@ window.onscroll = () => {
     navbar.classList.remove('active');
 }
 var typingEffect = new Typed(".multiText", {
-    strings : ["Software Engineer","Web Developer","Mobile Developer","Game Developer"],
-    loop : true,
-    typeSpeed : 100,
-    backSpeed : 80,
-    backDelay : 1500
+    strings: ["Software Engineer", "Web Developer", "Mobile Developer", "Game Developer"],
+    loop: true,
+    typeSpeed: 100,
+    backSpeed: 80,
+    backDelay: 1500
 })
